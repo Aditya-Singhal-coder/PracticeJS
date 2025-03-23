f6a7653979b471b581a5392540c2b889679e8802
Aditya-Singhal-coder <adityasinghal374@gmail.com>
2025-03-23 17:38:59 +0530

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+let form = document.querySelector('form');
 form.addEventListener('submit' , function(e){
     e.preventDefault()// khud se kuch bhi n kro
 
@@ -13,9 +13,9 @@ form.addEventListener('submit' , function(e){
         r.innerHTML = "Please give a valid weight"
     }
     else{
-       const bmi =  (w / ((h*h)/1000)).toFixed(2); // upto 2nd decimal
+       let bmi =  (w / ((h*h)/10000)).toFixed(2); // upto 2nd decimal
        // show the result
-       const extraInfo = "under"
+       let extraInfo = "under"
        if(bmi>0 && bmi <= 18.6){
             extraInfo = "Under Weight"
        }
@@ -25,6 +25,6 @@ form.addEventListener('submit' , function(e){
        else{
             extraInfo = "Over Weight"
        }
-       e.innerHTML = '<span>${bmi} and your weight lies in ${extraInfo}</span>';
+       r.innerHTML = '<span>${bmi} and your weight lies in ${extraInfo}</span>';
     }
 });
