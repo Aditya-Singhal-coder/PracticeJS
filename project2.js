bb54fe6ea650f1c22d70bdbed8413e5fe99fb1b6
Aditya-Singhal-coder <adityasinghal374@gmail.com>
2025-03-23 17:08:59 +0530

    Add files via upload
    First hands on javascript with basic project.

---
+++ project2.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+form.addEventListener('submit' , function(e){
+    e.preventDefault()// khud se kuch bhi n kro
+
+    const h = parseInt(document.querySelector('#height').value);
+    const w = parseInt(document.querySelector('#weight').value);
+    const r = document.querySelector('#results');
+    // some checks required so that code work properly
+    if(h === '' || h<0 || isNaN(h)){
+        r.innerHTML = "Please give a valid height"
+    }
+    else if(w === '' || w<0 || isNaN(w)){
+        r.innerHTML = "Please give a valid weight"
+    }
+    else{
+       const bmi =  (w / ((h*h)/1000)).toFixed(2); // upto 2nd decimal
+       // show the result
+       const extraInfo = "under"
+       if(bmi>0 && bmi <= 18.6){
+            extraInfo = "Under Weight"
+       }
+       else if(bmi>18.6 && bmi<=24.9){
+            extraInfo = "Normal Weight"
+       }
+       else{
+            extraInfo = "Over Weight"
+       }
+       e.innerHTML = '<span>${bmi} and your weight lies in ${extraInfo}</span>';
+    }
+});
